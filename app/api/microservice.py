6ae5af8c94284d88e5ef91590c7bf73e7e38e22e
MaiyoiaGamal <maiamer457@gmail.com>
2024-08-20 20:12:52 +0300

    Add validation on update and create and solve the code value if the name has many spaces

--- app/api/microservice.py
+++ app/api/microservice.py
@@ -4,7 +4,7 @@ from app.crud import CRUDMicroservice, CRUDMicroserviceTeamScorecard ,CRUDTeam, 
 from typing import List
 from app import dependencies 
 from pydantic import BaseModel, Field
-
+import re
 
 class Value(BaseModel):
     value: int | bool | float | str = Field(..., title="Value", description="Value of the service")
@@ -37,6 +37,18 @@ def create_microservice(newmicroservice: MicroserviceCreateApi,
                         servicescorecard: CRUDMicroserviceScoreCard = Depends(dependencies.getMicroserviceScoreCardsCrud),
                         scorecard: CRUDScoreCard = Depends(dependencies.getScoreCardsCrud)):
     
+    if not newmicroservice.name:
+        raise HTTPException(status_code=400, detail="Name cannot be empty")
+    
+    if len(newmicroservice.name) < 3:
+        raise HTTPException(status_code=400, detail="Name must be at least 3 characters long")
+    
+    if not newmicroservice.description :
+        raise HTTPException(status_code=400, detail="Description cannot be empty")
+    
+    if len(newmicroservice.description) > 500:
+        raise HTTPException(status_code=400, detail="Description cannot exceed 500 characters")
+ 
     try:
         teamobj = teamservice.get(newmicroservice.teamId)
         if teamobj is None:
@@ -49,13 +61,16 @@ def create_microservice(newmicroservice: MicroserviceCreateApi,
         try:
             scorecard.get(scorecardid)
         except Exception as x:
-            error_message = f'ScoreCard {scorecardid} was not found'
+            error_message = f'ScoreCard of ID: {scorecardid} was not found'
             raise HTTPException(status_code=404, detail=error_message)
-        
+    
+    code = newmicroservice.name.replace(" ","-") 
+    code = re.sub(r'-+', '-', code)
+
     created_microservice = microservice.create(MicroserviceCreate(name=newmicroservice.name,
                                             description=newmicroservice.description,
                                             teamId=newmicroservice.teamId,
-                                            code=newmicroservice.name.replace(" ","_")))
+                                            code=code))
     
      # Create microservice scorecard associations
     for scorecard in newmicroservice.scorecardids:
@@ -73,6 +88,19 @@ def update_microservice(microservice_id: int,updatemicroservice: MicroserviceCre
                         teamservice: CRUDTeam = Depends(dependencies.getTeamsCrud),
                         servicescorecard: CRUDMicroserviceScoreCard = Depends(dependencies.getMicroserviceScoreCardsCrud),
                         scorecard: CRUDScoreCard = Depends(dependencies.getScoreCardsCrud)):
+    
+    if not updatemicroservice.name:
+        raise HTTPException(status_code=400, detail="Name cannot be empty")
+    
+    if len(updatemicroservice.name) < 3:
+        raise HTTPException(status_code=400, detail="Name must be at least 3 characters long")
+
+    if not updatemicroservice.description :
+        raise HTTPException(status_code=400, detail="Description cannot be empty")
+
+    if len(updatemicroservice.description) > 500:
+        raise HTTPException(status_code=400, detail="Description cannot exceed 500 characters")
+ 
     try:
         teamobj = teamservice.get(updatemicroservice.teamId)
         if teamobj is None:
@@ -84,14 +112,16 @@ def update_microservice(microservice_id: int,updatemicroservice: MicroserviceCre
         try:
             scorecard.get(scorecardid)
         except Exception as x:
-            error_message = f'ScoreCard {scorecardid} was not found'
+            error_message = f'ScoreCard of ID: {scorecardid} was not found'
             raise HTTPException(status_code=404, detail=error_message)
-            
+        
+    code = updatemicroservice.name.replace(" ","-") 
+    code = re.sub(r'-+', '-', code) 
     updated_microservice = microservice.update(microservice_id, MicroserviceUpdate(
         name=updatemicroservice.name,
         description=updatemicroservice.description,
         teamId=updatemicroservice.teamId,
-        code=updatemicroservice.name.replace(" ", "_")
+        code=code
     ))
     
     servicescorecard.deleteByServiceId(microservice_id)
