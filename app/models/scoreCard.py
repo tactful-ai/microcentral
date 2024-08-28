86f9b3454a608f09e5df16c63aab9da0bd92d38b
Mazen Mohamed Nayef <mazennayef2002@gmail.com>
2024-08-28 16:00:55 +0300

    1- Changed the db of the scorecard and put the "code" field in it

--- app/models/scoreCard.py
+++ app/models/scoreCard.py
@@ -1,9 +1,10 @@
 from app.database.base_class import Base
 from sqlalchemy import Column, Integer, String
 
-# change
+
 class Scorecard(Base):
     id = Column(Integer, primary_key=True, index=True)
+    code = Column(String, nullable=False)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
 
