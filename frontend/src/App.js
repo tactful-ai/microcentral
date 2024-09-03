976e453dad2ea7396907012e7997076bb6c96212
Ammar-Radwan <ammarradwan03@gmail.com>
2024-09-03 11:57:44 +0300

    fixing backend runtime errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MetricCreateEdit from './pages/MetricCreateEdit';
 import Services from './pages/Services.jsx';
-import ScorecardCreateEdit from './pages/ScorecardCreateEdit';
 import Metrics from './pages/Metrics';
 
 function App() {
@@ -15,8 +14,6 @@ function App() {
         <Route path="/dashboard/metrics/create" element={<MetricCreateEdit mode="create"/>}/>
         <Route path="/dashboard/metrics/view/:metric_id" element={<MetricCreateEdit mode="view"/>}/>
         <Route path="/dashboard/metrics/edit/:metric_id" element={<MetricCreateEdit mode="edit"/>}/>
-        <Route path="/dashboard/scorecards/create" element={<ScorecardCreateEdit mode="create"/>}/>
-        <Route path="/dashboard/scorecards/edit/:scorecard_id" element={<ScorecardCreateEdit mode="edit"/>}/>
         {/* <Route element={"404 Not Found"} />*/} 
       </Routes>
     </BrowserRouter>
