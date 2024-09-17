9068b333435bdd70ffd7e267cf5bcb8ca863228b
MaiyoiaGamal <maiamer457@gmail.com>
2024-09-17 12:21:06 +0300

    update

--- app/crud/serviceMetric.py
+++ app/crud/serviceMetric.py
@@ -1,12 +1,13 @@
-from sqlalchemy.orm import Session 
+from sqlalchemy.orm import Session
 from sqlalchemy import func, and_
 from ..models import ServiceMetric, scoreCardMetrics
-from ..schemas import ServiceMetricCreate, ServiceMetricUpdate , ServiceMetricReading
+from ..schemas import ServiceMetricCreate, ServiceMetricUpdate, ServiceMetricReading
 from .base import CRUDBase
 from . import CRUDScoreCardMetric, CRUDMetric
 from app.utils.utility_functions import main_calculate_error, calculate_score
 from typing import Optional
 from datetime import datetime
+
 
 class CRUDServiceMetric(CRUDBase[ServiceMetric, ServiceMetricCreate, ServiceMetricUpdate]):
     def __init__(self, db_session: Session):
@@ -20,50 +21,34 @@ class CRUDServiceMetric(CRUDBase[ServiceMetric, ServiceMetricCreate, ServiceMetr
     def getByServiceId(self, serviceId: int) -> list[ServiceMetric]:
         return self.db_session.query(ServiceMetric).filter(ServiceMetric.serviceId == serviceId).all()
 
-    
-    def get_metric_values_by_service(self, service_id: int,from_date: Optional[datetime], to_date: Optional[datetime]) -> list[ServiceMetricReading]:
-        
-        query = self.db_session.query(ServiceMetric.metricId, ServiceMetric.value, ServiceMetric.timestamp).filter(ServiceMetric.serviceId == service_id)  
+    def get_metric_values_by_service(self, service_id: int, from_date: Optional[datetime], to_date: Optional[datetime]) -> list[ServiceMetricReading]:
+
+        query = self.db_session.query(ServiceMetric.metricId, ServiceMetric.value, ServiceMetric.timestamp).filter(
+            ServiceMetric.serviceId == service_id)
         if from_date and to_date:
-          query = query.filter(ServiceMetric.timestamp >= from_date, ServiceMetric.timestamp <= to_date)
+            query = query.filter(ServiceMetric.timestamp >=
+                                 from_date, ServiceMetric.timestamp <= to_date)
         elif from_date:
-          query = query.filter(ServiceMetric.timestamp >= from_date)
+            query = query.filter(ServiceMetric.timestamp >= from_date)
         elif to_date:
-          query = query.filter(ServiceMetric.timestamp <= to_date)
-        
+            query = query.filter(ServiceMetric.timestamp <= to_date)
+
         query = query.order_by(ServiceMetric.timestamp.desc())
         metrics = query.all()
         return metrics
-    
-      
-    def get_last_metrics(self, scorecard_id: int, service_id: int)-> list[ServiceMetric]:
-        subquery = (
-        self.db_session.query(
-            ServiceMetric.metricId,
-            func.max(ServiceMetric.timestamp).label('latest_timestamp')
-        )
-        .filter(
-            ServiceMetric.serviceId == service_id,
-            ServiceMetric.metricId.in_(self.scorecardMetrics.getMetricByScoreCradId(scorecard_id))
-        )
-        .group_by(ServiceMetric.metricId)
-        .subquery()
-    )
-        latest_metrics = self.db_session.query(
-        ServiceMetric.metricId,
-        ServiceMetric.value,
-        ServiceMetric.timestamp
-     ).join(
-        subquery,
-        and_(
-            ServiceMetric.metricId == subquery.c.metricId,
-            ServiceMetric.timestamp == subquery.c.latest_timestamp
-        )
-     ).all()
 
-        return latest_metrics
-     
-    
+    def get_last_metrics(self, scorecard_id: int, service_id: int) -> list[ServiceMetric]:
+        subquery = self.db_session.query(
+            ServiceMetric.metricId,
+            ServiceMetric.value,
+            ServiceMetric.timestamp
+        ).filter(
+            ServiceMetric.serviceId == service_id,
+            ServiceMetric.metricId.in_(
+                self.scorecardMetrics.getMetricByScoreCradId(scorecard_id))
+        ).order_by(ServiceMetric.metricId, ServiceMetric.timestamp.desc()).distinct(ServiceMetric.metricId).all()
+        return subquery
+
     def get_timestamp(self, service_id: int, scorecard_id: int):
         subquery = self.scorecardMetrics.getMetricByScoreCradId(scorecard_id)
 
@@ -78,41 +63,20 @@ class CRUDServiceMetric(CRUDBase[ServiceMetric, ServiceMetricCreate, ServiceMetr
             return formatted_time
         else:
             return None
- 
 
     def get_calculated_value(self, service_id: int, scorecard_id: int):
-        scorecard_metrics = self.scorecardMetrics.get_metrics(scorecard_id)
-        #metric_info_dict = {
-        #    metric.metricId: (
-        #        metric.criteria, metric.desiredValue, metric.weight)
-        #    for metric in scorecard_metrics
-        #}
+        scorecard_metrics = self.scorecardMetrics.get_metric(scorecard_id)
         metric_info_dict = {
-            metric.metricId (
+
+            metric.metricId: (
                 metric.criteria,
                 metric.desiredValue,
                 metric.weight,
-                metric_type = self.Metric.get(metric.metricId)
+                self.Metric.get(metric.metricId).type
             )
             for metric in scorecard_metrics
-            }
-
-        service_metrics = self.db_session.query(
-            ServiceMetric.metricId,
-            ServiceMetric.value,
-            ServiceMetric.timestamp
-        ).filter(
-            ServiceMetric.serviceId == service_id,
-            ServiceMetric.metricId.in_(metric_info_dict)
-
-        ).group_by(
-        ServiceMetric.metricId,  
-        ServiceMetric.value 
-        ).order_by(
-        ServiceMetric.metricId,ServiceMetric.timestamp.desc()).all()
-        #).distinct(ServiceMetric.metricId).all()
-        
-        
+        }
+        service_metrics = self.get_last_metrics(scorecard_id , service_id)
         scorevalue = 0
         for service_metric in service_metrics:
             criteria, desired_value, weight, metric_type = metric_info_dict.get(
@@ -122,8 +86,6 @@ class CRUDServiceMetric(CRUDBase[ServiceMetric, ServiceMetricCreate, ServiceMetr
 
                 error = main_calculate_error(
                     service_metric.value, desired_value, weight, criteria, metric_type)
-                score = calculate_score(error)
+                score = calculate_score(error, weight)
                 scorevalue += score
-                return scorevalue
-
-        return None
+            return scorevalue
