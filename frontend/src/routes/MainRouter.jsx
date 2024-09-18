5cb7fd97d672c9c7bbd44caa52538b023c26fbd4
Ammar-Radwan <ammarradwan03@gmail.com>
2024-09-18 14:40:58 +0300

    Additional edit for removing not needed pages

--- frontend/src/routes/MainRouter.jsx
+++ frontend/src/routes/MainRouter.jsx
@@ -2,54 +2,38 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 // All Pages
-import Services from '../pages/Services.jsx';
 import Metrics from '../pages/Metrics';
 import MetricCreateEdit from '../pages/MetricCreateEdit';
-import Scorecards from '../pages/Scorecards';
 import NotFound from '../pages/NotFound';
 
 const MainRouter = createBrowserRouter([
-    // Services routes
-    {
-      path: "/",
-      element: <Services />
-    },
-    {
-        path: "/dashboard",
-        children: [
-            {
-                path: "services",
-                element: <Services />,
-            },
-            // Scorecards routes
-            {
-              path: "scorecards",
-              element: <Scorecards />
-            },
-            // Metrics routes
-            {
-              path: "metrics",
-              element: <Metrics />
-            },
-            {
-              path: "metrics/create",
-              element: <MetricCreateEdit mode="create"/>
-            },
-            {
-              path: "metrics/edit/:metric_id",
-              element: <MetricCreateEdit mode="edit"/>
-            },
-        ]
-    },
-    // Not Found
-    {
-        path: "/404",
-        element: <NotFound />
-    },
-    {
-        path: "*",
-        element: <NotFound />
-    },
+  {
+    path: "/dashboard",
+    children: [
+      // Metrics routes
+      {
+        path: "metrics",
+        element: <Metrics />
+      },
+      {
+        path: "metrics/create",
+        element: <MetricCreateEdit mode="create"/>
+      },
+      {
+        path: "metrics/edit/:metric_id",
+        element: <MetricCreateEdit mode="edit"/>
+      },
+    ]
+  },
+  // Not Found
+  {
+    path: "/404",
+    element: <NotFound />
+  },
+  {
+    path: "*",
+    element: <NotFound />
+  },
 ]);
 
 export default MainRouter;
