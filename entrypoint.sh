b1a73da7217b69c790d7182f85a80b23ee8b975d
norhan ali <noraali5800@gmail.com>
2024-09-23 16:26:04 +0300

    make some changes to pipline

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,9 @@ fi
 
 # Run migrations to create tables
 #poetry run python app/manager.py init_db
-echo "Running migrations..."
-poetry run alembic revision --autogenerate -m "Initial migration"
-echo "Revision created"
 poetry run alembic upgrade head
 echo "Migrations complete"
 
 # Seed the database
-poetry run python /app/initial_data.py
 
 exec "$@"
